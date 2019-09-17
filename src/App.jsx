0d5483ff75473846067558685ed68cbb1daf7d16
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-09-17 18:07:51 -0300

    Restarting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                </nav>
+
+                <nav class="navbar navbar-light bg-light">
+                    <span class="navbar-brand mb-0 h1">Navbar</span>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default App;
